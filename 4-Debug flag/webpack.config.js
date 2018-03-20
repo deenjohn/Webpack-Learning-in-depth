f7f173aa4ad7b55e6a54c07eaff95fd8d0af2f46
Deen John <dj.itbhu@gmail.com>
2018-03-20 06:44:01 +0530

    Add files via upload

---
+++ 4-Debug flag/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+
+let baseConfig = {
+  entry: "./app/app.js",
+  output: {
+    path: path.resolve(__dirname, "app/dist"),
+    filename: "app.bundle.js",
+    publicPath: "/dist/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "env",
+                {
+                  debug: true
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "app"),
+    compress: false,
+    hotOnly: true,
+    port: 3000
+  },
+  plugins: []
+};
+
+module.exports = function(env) {
+  let isDev = env == "development";
+  if (isDev) {
+    return merge(baseConfig, {
+      plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+      ]
+    });
+  }
+};
