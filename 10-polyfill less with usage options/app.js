29b0f63d19594807cd3d2cbe3dbe10ca2dcc66b7
Deen John <dj.itbhu@gmail.com>
2018-03-21 08:59:38 +0530

    Add files via upload

---
+++ 10-polyfill less with usage options/app.js
@@ -0,0 +1,7 @@
+//import "babel-polyfill";
+import "./klondike/scoring.js";
+// import "./klondike/klondike.js";
+// import "./klondike/board.js";
+// import "./klondike/game.js";
+
+angular.module("solitaire", ["klondike", "ngDraggable"]);
